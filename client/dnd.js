5ba9fb1505b02cfd0f5e7a9f26d453907f396f2e
Joe Kratzat <jjkratzat@gmail.com>
2012-05-03 20:34:25 -0400

    Broke the client and server code out into client/ and server/ directories.

---
+++ client/dnd.js
@@ -0,0 +1,35 @@
+CharacterList = new Meteor.Collection("characters");
+
+Template.characters.events = {
+  'click #add_button': function () {
+    console.debug(parseInt($("#new-Initiative").val()));
+    var obj = CharacterList.findOne({name:$("#new-character").val()});
+    if(obj == null){
+      CharacterList.insert({
+        name:$("#new-character").val(),
+        initiative:parseInt($("#new-Initiative").val())
+      });
+      $("#new-character").val("");
+      $("#new-Initiative").val("");
+    }else{
+      CharacterList.update({name:$("#new-character").val()},{$set: {initiative:parseInt($("#new-Initiative").val())}});
+    }
+  }
+};
+
+
+Template.character_list.characters = function () {
+  return CharacterList.find({}, {sort: {initiative: -1}});
+};
+
+Template.character.events = {
+  'click .delete': function () {
+    console.debug('delete'+this);
+    CharacterList.remove(this._id);
+  },
+  'click .char_name': function (){
+    console.debug('edit'+this);
+    $("#new-character").val(this.name);
+    $("#new-Initiative").val(this.initiative);
+  }
+};
