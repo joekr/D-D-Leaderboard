99f9160422df9d36d15a5202b9d045ee537863fd
Joe Kratzat <jjkratzat@gmail.com>
2012-05-07 20:34:37 -0400

    Added Twitter Bootstrap.
    Spacebar now displays which character's turn it is.

--- client/dnd.js
+++ client/dnd.js
@@ -7,7 +7,8 @@ Template.characters.events = {
     if(obj == null){
       CharacterList.insert({
         name:$("#new-character").val(),
-        initiative:parseInt($("#new-Initiative").val())
+        initiative:parseInt($("#new-Initiative").val()),
+        active:false
       });
       $("#new-character").val("");
       $("#new-Initiative").val("");
@@ -16,7 +17,6 @@ Template.characters.events = {
     }
   }
 };
-
 
 Template.character_list.characters = function () {
   return CharacterList.find({}, {sort: {initiative: -1}});
@@ -28,8 +28,43 @@ Template.character.events = {
     CharacterList.remove(this._id);
   },
   'click .char_name': function (){
-    console.debug('edit'+this);
+    console.debug('edit'+this.active);
     $("#new-character").val(this.name);
     $("#new-Initiative").val(this.initiative);
   }
 };
+
+var hasActiveChar = function(){
+  return (CharacterList.find({active: true}).count() > 0);
+}
+
+var activeRecord = function(){
+  return CharacterList.findOne({active:true});
+}
+
+$(document).keydown(function(evt) {
+if (evt.keyCode == 32) {
+  // GROSS!!! but it works for now
+      var cur = CharacterList.find({}, {sort: {initiative: -1}});
+      var nextShouldBeActive = false;
+      var skip = false;
+      var hasOneActive = hasActiveChar();
+      var activeChar = activeRecord();
+
+      cur.forEach(function(c){        
+        console.debug(JSON.stringify(c)+" - ");
+        if(nextShouldBeActive || !hasOneActive){
+          CharacterList.update(c,{$set: {active:true}});
+          skip = true;
+          hasOneActive = true;
+          nextShouldBeActive = false;
+        }
+
+        if(c._id == activeChar._id){
+          CharacterList.update(c,{$set: {active:false}});
+          nextShouldBeActive = true;
+        }    
+        skip = false;              
+      });
+  }
+});
--- dnd.js
+++
@@ -1,8 +0,0 @@
-CharacterList = new Meteor.Collection("characters");
-Meteor.publish('characters', function () {
-  return Lists.find();
-});
-
-Meteor.startup(function () {
-  // code to run on server at startup
-});
