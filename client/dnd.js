5fe72c9807227ae8216b5596bd08e7fb746a801e
Sarah Vessels <cheshire137@gmail.com>
2012-05-09 23:25:09 -0400

    Formatting code, styling character form
    Making use of Bootstrap navbar style in new-character form.  Using
    Markdown in README.  Formatting Javascript, reducing code duplication a
    little.

--- client/dnd.js
+++ client/dnd.js
@@ -1,70 +1,92 @@
 CharacterList = new Meteor.Collection("characters");
 
 Template.characters.events = {
-  'click #add_button': function () {
-    console.debug(parseInt($("#new-Initiative").val()));
-    var obj = CharacterList.findOne({name:$("#new-character").val()});
-    if(obj == null){
-      CharacterList.insert({
-        name:$("#new-character").val(),
-        initiative:parseInt($("#new-Initiative").val()),
-        active:false
-      });
-      $("#new-character").val("");
-      $("#new-Initiative").val("");
-    }else{
-      CharacterList.update({name:$("#new-character").val()},{$set: {initiative:parseInt($("#new-Initiative").val())}});
-    }
-  }
+	'click #add-button': function () {
+		var newInitiative = $("#new-initiative");
+		var newChar = $("#new-character");
+		console.debug(parseInt(newInitiative.val(), 10));
+		var obj = CharacterList.findOne({name:newChar.val()});
+		if (null == obj){
+			CharacterList.insert({
+				name: newChar.val(),
+				initiative: parseInt(newInitiative.val(), 10),
+				active: false
+			});
+			newChar.val("");
+			newInitiative.val("");
+		} else {
+			CharacterList.update({
+				name:newChar.val()
+			}, {
+				$set: {
+					initiative:parseInt(newInitiative.val(), 10)
+				}
+			});
+		}
+	}
 };
 
 Template.character_list.characters = function () {
-  return CharacterList.find({}, {sort: {initiative: -1,name:1}});
+	return CharacterList.find(
+		{},
+		{
+			sort: {
+				initiative: -1,
+				name:1
+			}
+		}
+	);
 };
 
 Template.character.events = {
-  'click .delete': function () {
-    console.debug('delete'+this);
-    CharacterList.remove(this._id);
-  },
-  'click .char_name': function (){
-    console.debug('edit'+this.active);
-    $("#new-character").val(this.name);
-    $("#new-Initiative").val(this.initiative);
-  }
+	'click .delete': function() {
+		console.debug('delete' + this);
+		CharacterList.remove(this._id);
+	},
+	'click .char_name': function(){
+		console.debug('edit' + this.active);
+		$("#new-character").val(this.name);
+		$("#new-initiative").val(this.initiative);
+	}
 };
 
 var hasActiveChar = function(){
-  return (CharacterList.find({active: true}).count() > 0);
+	return (CharacterList.find({active: true}).count() > 0);
 }
 
 var activeRecord = function(){
-  return CharacterList.findOne({active:true});
+	return CharacterList.findOne({active: true});
 }
 
 $(document).keydown(function(evt) {
-if (evt.keyCode == 32) {
-  // GROSS!!! but it works for now
-      var cur = CharacterList.find({}, {sort: {initiative: -1,name:1}});
-      var nextShouldBeActive = false;
-      var skip = false;
-      var hasOneActive = hasActiveChar();
-      var activeChar = activeRecord();
-
-      cur.forEach(function(c){        
-        console.debug(JSON.stringify(c)+" - ");
-        if(nextShouldBeActive || !hasOneActive){
-          CharacterList.update(c,{$set: {active:true}});
-          skip = true;
-          hasOneActive = true;
-          nextShouldBeActive = false;
-        }
-
-        if(c._id == activeChar._id){
-          CharacterList.update(c,{$set: {active:false}});
-          nextShouldBeActive = true;
-        }    
-        skip = false;              
-      });
-  }
+	if (evt.keyCode == 32) {
+		// GROSS!!! but it works for now
+		var cur = CharacterList.find(
+			{},
+			{
+				sort: {
+					initiative: -1,
+					name: 1
+				}
+			}
+		);
+		var nextShouldBeActive = false;
+		var skip = false;
+		var hasOneActive = hasActiveChar();
+		var activeChar = activeRecord();
+		cur.forEach(function(c) {        
+			console.debug(JSON.stringify(c) + " - ");
+			if (nextShouldBeActive || !hasOneActive){
+				CharacterList.update(c, {$set: {active: true}});
+				skip = true;
+				hasOneActive = true;
+				nextShouldBeActive = false;
+			}
+			if (undefined != c && undefined != activeChar && c._id == activeChar._id){
+				CharacterList.update(c, {$set: {active: false}});
+				nextShouldBeActive = true;
+			}    
+			skip = false;              
+		});
+	}
 });
