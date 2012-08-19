12162dfe39da084b81ffdc264214d09cd818e0b6
Sarah Vessels <cheshire137@gmail.com>
2012-08-18 23:10:48 -0400

    Fixing new char bug
    Adding a new character sets all their defense properties too.
    Disallowing add of a char with no name.  Rearranging HTML grouping and
    templates.  Moving navbar out of characters template.  Using TextMate
    to reformat JS file.

--- client/dnd.js
+++ client/dnd.js
@@ -1,87 +1,107 @@
 CharacterList = new Meteor.Collection("characters");
 
 var playCharacterName = function(charName) {
-	//console.debug("Going to play character audio " + charName);
-	var audio = $('#char-name-audio');
-	var audioPlayer = audio.get(0);
-	if (typeof audio.attr('src') != 'undefined') {
-		audioPlayer.pause();
-		audioPlayer.currentTime = 0;
-	}
-	var audibleNames = ['xavia', 'thar', 'dragon'];
-	var mp3Path = '/' + charName + '.mp3';
-	//var m4aPath = '/' + charName + '.m4a';
-	if ($.inArray(charName, audibleNames) > -1) {
-		audio.attr('src', mp3Path);
-		//console.debug(audio);
-		audioPlayer.play();
-	} else if ('enemy' == charName ||
-			'enemies' == charName ||
-			'baddies' == charName
-            || 'zombies' == charName
-            || 'hobgoblins' == charName) {
+		//console.debug("Going to play character audio " + charName);
+		var audio = $('#char-name-audio');
+		var audioPlayer = audio.get(0);
+		if (typeof audio.attr('src') != 'undefined') {
+			audioPlayer.pause();
+			audioPlayer.currentTime = 0;
+		}
+		var audibleNames = ['xavia', 'thar', 'dragon'];
+		var enemyNames = ['enemy', 'enemies', 'baddies', 'zombies', 'hobgoblins'];
+		var mp3Path = '/' + charName + '.mp3';
+		//var m4aPath = '/' + charName + '.m4a';
+		if ($.inArray(charName, audibleNames) > -1) {
+			audio.attr('src', mp3Path);
+			//console.debug(audio);
+			audioPlayer.play();
+		} else if ($.inArray(charName, enemyNames) > -1) {
+			audio.attr('src', '/dragon.mp3');
+			audioPlayer.play();
+		} else {
+			audio.attr('src', '/next_character.mp3');
+			audioPlayer.play();
+		}
+	};
 
-		audio.attr('src', '/dragon.mp3');
-		audioPlayer.play();
-	}else{
-    audio.attr('src', '/next_character.mp3');
-    audioPlayer.play();
-  }
-};
-
-var nextCharacter = function() {
-	var setActiveTr = function(tr) {
+var setActiveTr = function(tr) {
 		tr.addClass('active');
-		CharacterList.update({_id: tr.attr('id')}, {$set: {active: true}});
+		CharacterList.update({
+			_id: tr.attr('id')
+		}, {
+			$set: {
+				active: true
+			}
+		});
 		var activeCharName = $('.char-name', tr).text().toLowerCase();
 		playCharacterName(activeCharName);
 	};
-	var tr = $('#character-table tbody tr.active');
-	if (tr.length < 1) {
-		// No current active character, start with the first in the table,
-		// i.e., the one with highest initiative
-		setActiveTr($('#character-table tbody tr:first-child'));
-	} else {
-		// Move to next character in the table, i.e., the one with the
-		// next highest initiative
-		tr.removeClass('active');
-		CharacterList.update({_id: tr.attr('id')}, {$set: {active: false}});
-		var nextTr = tr.next();
-		if (nextTr.length < 1) {
-			nextTr = tr.parent().children('tr:first-child');
-		}
-		setActiveTr(nextTr);
-	}
-};
 
-Template.characters.events = {
+var nextCharacter = function() {
+		var tr = $('#character-table tbody tr.active');
+		if (tr.length < 1) {
+			// No current active character, start with the first in the table,
+			// i.e., the one with highest initiative
+			setActiveTr($('#character-table tbody tr:first-child'));
+		} else {
+			// Move to next character in the table, i.e., the one with the
+			// next highest initiative
+			tr.removeClass('active');
+			CharacterList.update({
+				_id: tr.attr('id')
+			}, {
+				$set: {
+					active: false
+				}
+			});
+			var nextTr = tr.next();
+			if (nextTr.length < 1) {
+				nextTr = tr.parent().children('tr:first-child');
+			}
+			setActiveTr(nextTr);
+		}
+	};
+
+Template.navbar.events = {
 	'click #add-button': function() {
-		var newInitiative = $("#new-initiative");
-		var newChar = $("#new-character");
-		var newEnemy = $('#new-enemy');
-		var inGame = $('#in-game');
 		var id = $('#character-id').val();
-		var obj = CharacterList.findOne({_id: id});
-		var isEnemy = newEnemy.is(':checked');
-		var isInGame = inGame.is(':checked');
-		var initiativeVal = parseInt(newInitiative.val(), 10);
-		var name = newChar.val();
-    var charAc = parseInt($("#char-ac").val());
-    var charFort = parseInt($("#char-fort").val());
-    var charRef = parseInt($("#char-ref").val());
-    var charWill = parseInt($("#char-will").val());
-    var damage = parseInt($("#char-dmg").val());
-		if (null == obj){
-			CharacterList.insert({
+		var obj = CharacterList.findOne({
+			_id: id
+		});
+		var nameField = $("#new-character");
+		var name = nameField.val();
+		var nameControlGroup = nameField.closest('.control-group');
+		if ($.trim(name) == '') {
+			nameControlGroup.addClass('error');
+			return;
+		}
+		nameControlGroup.removeClass('error');
+		var isEnemy = $('#new-enemy').is(':checked');
+		var isInGame = $('#in-game').is(':checked');
+		var initiativeVal = parseInt($("#new-initiative").val(), 10);
+		var charAc = parseInt($("#char-ac").val(), 10);
+		var charFort = parseInt($("#char-fort").val(), 10);
+		var charRef = parseInt($("#char-ref").val(), 10);
+		var charWill = parseInt($("#char-will").val(), 10);
+		var damage = parseInt($("#char-dmg").val(), 10);
+		if (null == obj) {
+			var charProps = {
 				name: name,
 				initiative: initiativeVal,
 				active: false,
 				isEnemy: isEnemy,
-				char_in_game: true
-				//TODO: need to build out object on new
-			});
+				char_in_game: isInGame,
+				char_ac: charAc,
+				char_fort: charFort,
+				char_ref: charRef,
+				char_will: charWill,
+				damage: damage
+			};
+			console.log("Inserting new character: " + JSON.stringify(charProps));
+			CharacterList.insert(charProps);
 		} else {
-			console.debug("Updating ID " + id + " " +isInGame);
+			console.debug("Updating character #" + id);
 			CharacterList.update({
 				_id: id
 			}, {
@@ -89,20 +109,20 @@ Template.characters.events = {
 					initiative: initiativeVal,
 					isEnemy: isEnemy,
 					name: name,
-          char_ac: charAc,
-          char_fort: charFort,
-          char_ref: charRef,
-          char_will: charWill,
-          damage: damage,
-          char_in_game: isInGame
+					char_ac: charAc,
+					char_fort: charFort,
+					char_ref: charRef,
+					char_will: charWill,
+					damage: damage,
+					char_in_game: isInGame
 				}
 			});
-		}	
+		}
 		$('#reset-button').click();
 	},
 	'click #reset-button': function() {
 		$('#new-enemy').attr('checked', false);
-		$('#in-game').attr('checked', false);
+		$('#in-game').attr('checked', true);
 		$('#add-button').val('Add');
 		$('#character-id').val('');
 	},
@@ -112,16 +132,15 @@ Template.characters.events = {
 	}
 };
 
-Template.character_list.characters = function () {
-	return CharacterList.find(
-		{char_in_game: true},
-		{
-			sort: {
-				initiative: -1,
-				name: 1
-			}
+Template.character_list.characters = function() {
+	return CharacterList.find({
+		char_in_game: true
+	}, {
+		sort: {
+			initiative: -1,
+			name: 1
 		}
-	);
+	});
 };
 
 Template.character.events = {
@@ -135,11 +154,11 @@ Template.character.events = {
 		console.debug('edit' + this.active);
 		$("#new-character").val(this.name);
 		$("#new-initiative").val(this.initiative);
-    $("#char-ac").val(this.char_ac);
-    $("#char-fort").val(this.char_fort);
-    $("#char-ref").val(this.char_ref);
-    $("#char-will").val(this.char_will); 
-    $("#char-dmg").val(this.damage);    
+		$("#char-ac").val(this.char_ac);
+		$("#char-fort").val(this.char_fort);
+		$("#char-ref").val(this.char_ref);
+		$("#char-will").val(this.char_will);
+		$("#char-dmg").val(this.damage);
 		$('#new-enemy').attr('checked', this.isEnemy);
 		$('#in-game').attr('checked', this.char_in_game);
 		$('#character-id').val(this._id);
@@ -147,16 +166,15 @@ Template.character.events = {
 	}
 };
 
-Template.out_game_character_list.characters = function () {
-	return CharacterList.find(
-		{char_in_game: false},
-		{
-			sort: {
-				initiative: -1,
-				name: 1
-			}
+Template.out_game_character_list.characters = function() {
+	return CharacterList.find({
+		char_in_game: false
+	}, {
+		sort: {
+			initiative: -1,
+			name: 1
 		}
-	);
+	});
 };
 
 Template.out_game_character_list.events = {
@@ -170,11 +188,11 @@ Template.out_game_character_list.events = {
 		console.debug('edit' + this.active);
 		$("#new-character").val(this.name);
 		$("#new-initiative").val(this.initiative);
-    $("#char-ac").val(this.char_ac);
-    $("#char-fort").val(this.char_fort);
-    $("#char-ref").val(this.char_ref);
-    $("#char-will").val(this.char_will); 
-    $("#char-dmg").val(this.damage);    
+		$("#char-ac").val(this.char_ac);
+		$("#char-fort").val(this.char_fort);
+		$("#char-ref").val(this.char_ref);
+		$("#char-will").val(this.char_will);
+		$("#char-dmg").val(this.damage);
 		$('#new-enemy').attr('checked', this.isEnemy);
 		$('#in-game').attr('checked', this.char_in_game);
 		$('#character-id').val(this._id);
@@ -183,16 +201,20 @@ Template.out_game_character_list.events = {
 };
 
 var hasActiveChar = function() {
-	return (CharacterList.find({active: true}).count() > 0);
-};
+		return (CharacterList.find({
+			active: true
+		}).count() > 0);
+	};
 
 var activeRecord = function() {
-	return CharacterList.findOne({active: true});
-};
+		return CharacterList.findOne({
+			active: true
+		});
+	};
 
 $(document).keydown(function(evt) {
-	if (evt.keyCode == 32) {	
-		evt.preventDefault();	
+	if (evt.keyCode == 32) {
+		evt.preventDefault();
 		nextCharacter();
 	}
 });
